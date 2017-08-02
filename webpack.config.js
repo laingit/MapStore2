4691da0fd3910db036bb2dd2da92cd92d813e2f9
Andrea Lai <laidevpub@gmail.com>
2017-08-02 15:59:30 +0200

    webpack.config.js -> usa proxy locale in Tomcat

--- webpack.config.js
+++ webpack.config.js
@@ -129,10 +129,10 @@ module.exports = {
     devServer: {
         proxy: {
             '/mapstore/rest/geostore': {
-                target: "http://dev.mapstore2.geo-solutions.it"
+                target: "http://localhost:8080"
             },
             '/mapstore/proxy': {
-                target: "http://dev.mapstore2.geo-solutions.it"
+                target: "http://localhost:8080"
             },
             '/docs': {
                 target: "http://localhost:8081",
