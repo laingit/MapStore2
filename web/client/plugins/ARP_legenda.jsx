fcaef1cf45b3e32a60d7fb118113987779cedf1b
Andrea Lai <laidevpub@gmail.com>
2017-08-18 12:56:52 +0200

    connecting plugin to redux

--- web/client/plugins/ARP_legenda.jsx
+++ web/client/plugins/ARP_legenda.jsx
@@ -1,67 +1,50 @@
 const PropTypes = require('prop-types');
-/*
- * Copyright 2016, GeoSolutions Sas.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
-const assign = require('object-assign');
-const {UserDetails, PasswordReset, UserMenu, Login, LoginNav } = require('./ARP_legenda/index');
+const {connect} = require('react-redux');
+
+
 const {getLegendaEpics,
     trovaCodiciLegendaEpics} = require('../epics/ARP_legendaEpics');
 
-require('./login/login.css');
-
-/**
-  * Login Plugin. Allow to login/logout or show user info and reset password tools
-  * @class Login
-  * @memberof plugins
-  * @static
-  *
-  * @prop {string} cfg.id identifier of the Plugin, by default `"mapstore-login-menu"`
-  * @prop {object} cfg.menuStyle inline style for the menu, by defualt:
-  * ```
-  * menuStyle: {
-  *      zIndex: 30
-  * }
-  *```
-  */
 class ARPLegendaTool extends React.Component {
     static propTypes = {
         id: PropTypes.string,
-        menuStyle: PropTypes.object
+        style: PropTypes.object,
+        leg_full: PropTypes.array,
+        leg_partial: PropTypes.array
     };
 
     static defaultProps = {
-        id: "mapstore-login-menu",
-        menuStyle: {
-            zIndex: 30
+        id: "andrea-legenda",
+        style: {
+            zIndex: 50,
+            position: 'absolute',
+            top: '50px',
+            left: '50px',
+            "background-color": 'white'
         }
     };
 
     render() {
-        return (<div id={this.props.id}>
-            <div style={this.props.menuStyle}>
-                <UserMenu />
-            </div>
-            <UserDetails />
-        </div>);
+        return (
+        <div id={this.props.id} style={this.props.style}>
+           {this.props.leg_partial.map((item) => <div key={item}>{item}</div>)}
+        </div>
+        );
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        leg_full: state.ARP_legenda.ARP_legenda_full,
+        leg_partial: state.ARP_legenda.ARP_legenda_partial
+     };
+}
+
+const ARPLegendaToolRedux = connect(mapStateToProps)(ARPLegendaTool);
+
 module.exports = {
-    ARP_legendaPlugin: assign(ARPLegendaTool, {
-        OmniBar: {
-            name: "arpLegenda",
-            position: 5,
-            tool: LoginNav,
-            tools: [UserDetails],
-            priority: 1
-        }
-    }),
+    ARP_legendaPlugin: ARPLegendaToolRedux,
     reducers: {ARP_legenda: require('../reducers/ARP_legendaReducer')},
     epics: {
         getLegendaEpics,
