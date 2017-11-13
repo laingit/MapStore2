5bc874a04c580bb3de497b5185a42153b2ba6c7e
Andrea Lai <laidevpub@gmail.com>
2017-11-13 10:22:48 +0100

    add example data - Arp_legendaUtils.js

--- web/client/utils/Arp_legendaUtils.js
+++ web/client/utils/Arp_legendaUtils.js
@@ -7,9 +7,9 @@ function compareArrayOfString(a, b) {
     if (a > b) { return 1; }
     return 0;
 }
-
-function getParentsOfVaules(state, tabKey, values) {
-    const table = state[tabKey];
+// tabKey may be "liv0","liv1","liv2"
+function getParentsOfVaules(dataNormalizzata, tabKey, values) {
+    const table = dataNormalizzata[tabKey];
     if (!table) {
         throw "errore tabella non esiste: " + tabKey;
     }
@@ -57,6 +57,10 @@ function getParentsOfVaules(state, tabKey, values) {
     return selezionaCampi(riassunto, soloId);
 }
 
+// START-DOC----------------selezionaLegendaDaCartografati
+// example: listaCartografati = ["A1.2", "A1.3", "A2.3", "B2.1", "C1.2", "C1.3", "C1.4", "C1.8"]
+// vedi -> livDueGerarchia from '../DATA/liv_due_gerarchia.json';
+// let DATA_NORMALIZZATA = livDueGerarchia.data;
 function selezionaLegendaDaCartografati(dataNormalizzata, listaCartografati) {
     let liv2 = getParentsOfVaules(dataNormalizzata, "liv2", listaCartografati);
     let liv1 = getParentsOfVaules(dataNormalizzata, "liv1", Object.keys(liv2));
@@ -64,6 +68,48 @@ function selezionaLegendaDaCartografati(dataNormalizzata, listaCartografati) {
 
     return {liv0, liv1, liv2};
 }
+
+/* esempio_output_selezionaLegendaDaCartografati
+{
+    "liv0": {
+        "root": [
+            ["liv0", "A"], // example ident -> getFromDataNorm(data, ident) data = dataNormalizzata
+            ["liv0", "B"],
+            ["liv0", "C"]
+        ]
+    },
+    "liv1": {
+        "A": [
+            ["liv1", "A1"],
+            ["liv1", "A2"]
+        ],
+        "B": [
+            ["liv1", "B2"]
+        ],
+        "C": [
+            ["liv1", "C1"]
+        ]
+    },
+    "liv2": {
+        "A1": [
+            ["liv2", "A1.2"],
+            ["liv2", "A1.3"]
+        ],
+        "A2": [
+            ["liv2", "A2.3"]
+        ],
+        "B2": [
+            ["liv2", "B2.1"]
+        ],
+        "C1": [
+            ["liv2", "C1.2"],
+            ["liv2", "C1.3"],
+            ["liv2", "C1.4"],
+            ["liv2", "C1.8"]
+        ]
+    }
+};
+// END DOC--------------------- selezionaLegendaDaCartografati---------------------------------------*/
 
 function getFromDataNorm(data, ident) {
     let [tabella, key] = ident;
@@ -95,6 +141,119 @@ function createFlatten(dataNorm, legenda) {
     return root;
 }
 
+// START-DOC---------------- example_createFlatten_root_output
+/*
+[
+    {
+        "tag": "liv0",
+        "value":
+        {
+            "id": "A",
+            "desc": "Rocce magmatiche",
+            "dentro": "root"
+        }
+    },
+    {
+        "tag": "liv1",
+        "value":
+        {
+            "id": "A1",
+            "desc": "Rocce magmatiche intrusive",
+            "dentro": "A"
+        }
+    },
+    {
+        "tag": "liv2",
+        "value":
+        {
+            "rgb": "#E876B9",
+            "id": "A1.3",
+            "desc": "Granodioriti, Granodioriti tonalitiche, Microgranodioriti, Granodioriti monzogranitiche",
+            "dentro": "A1"
+        }
+    },
+    {
+        "tag": "liv1",
+        "value":
+        {
+            "id": "A2",
+            "desc": "Rocce magmatiche effusive",
+            "dentro": "A"
+        }
+    },
+    {
+        "tag": "liv2",
+        "value":
+        {
+            "rgb": "#AB773F",
+            "id": "A2.3",
+            "desc": "Basalti alcalini, Trachibasalti, Hawaiiti, Mugeariti, Fonoliti, Fonoliti tefritiche",
+            "dentro": "A2"
+        }
+    },
+    {
+        "tag": "liv0",
+        "value":
+        {
+            "id": "C",
+            "desc": "Rocce sedimentarie",
+            "dentro": "root"
+        }
+    },
+    {
+        "tag": "liv1",
+        "value":
+        {
+            "id": "C1",
+            "desc": "Rocce sedimentarie terrigene",
+            "dentro": "C"
+        }
+    },
+    {
+        "tag": "liv2",
+        "value":
+        {
+            "rgb": "#DDF2EA",
+            "id": "C1.2",
+            "desc": "Depositi terrigeni continentali di conoide e piana alluvionale (ghiaie, sabbie, limi, argille), (conglomerati, arenarie, siltiti, peliti) ",
+            "dentro": "C1"
+        }
+    },
+    {
+        "tag": "liv2",
+        "value":
+        {
+            "rgb": "#FFFFCF",
+            "id": "C1.3",
+            "desc": "Depositi terrigeni continentali legati a gravità (detriti di versante, frane, coltri eluvio-colluviali, \"debris avalanches\", brecce)",
+            "dentro": "C1"
+        }
+    },
+    {
+        "tag": "liv2",
+        "value":
+        {
+            "rgb": "#E6F5A4",
+            "id": "C1.4",
+            "desc": "Depositi terrigeni palustri, lacustri, lagunari (limi, argille limose, fanghi torbosi con materia organica anche con intercalazioni di sabbie, selci)",
+            "dentro": "C1"
+        }
+    },
+    {
+        "tag": "liv2",
+        "value":
+        {
+            "rgb": "#FFED00",
+            "id": "C1.8",
+            "desc": "Depositi terrigeni fluvio-deltizi (sabbie, microconglomerati, arenarie carbonatiche, siltiti argillose)",
+            "dentro": "C1"
+        }
+    }
+]
+// END DOC ------------------ example_createFlatten_root_output-----*/
+
+
+// not used so far
 function createTree(dataNorm, legenda) {
     let root = [];
     let iDsLiv0 = legenda.liv0.root;
@@ -125,7 +284,7 @@ function createTree(dataNorm, legenda) {
     return root;
 }
 
-
+// not used so far
 export function getTreeGerarchia(codiciTrovati) {
     if (codiciTrovati.length === 0) {
         return [];
@@ -135,6 +294,7 @@ export function getTreeGerarchia(codiciTrovati) {
     return createTree(DATA_NORMALIZZATA, legCartografato);
 }
 
+// example: codiciTrovati = ["A1.2", "A1.3", "A2.3", "B2.1", "C1.2", "C1.3", "C1.4", "C1.8"]
 export function getFlattenGerarchia(codiciTrovati) {
     if (codiciTrovati.length === 0) {
         return [];
@@ -145,6 +305,7 @@ export function getFlattenGerarchia(codiciTrovati) {
 }
 
 function creaLegendaTotaleLiv2Flatten() {
+    // Object.keys(DATA_NORMALIZZATA.liv2) = ["A1.1", "A1.2", ....", "B2.1", ... "C1.1", "C1.2", ...] ALL "liv2"
     let dominioLiv2 = Object.keys(DATA_NORMALIZZATA.liv2);
     dominioLiv2.sort(compareArrayOfString);
     return getFlattenGerarchia(dominioLiv2);
